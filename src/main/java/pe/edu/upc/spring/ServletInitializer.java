edbe2e2b9b487bfe8e8190be844418744fbfad3a
Moisés Jacob Reyna Arista <u201517018@upc.edu.pe>
2021-06-17 05:33:55 -0500

    Update ServletInitializer.java

--- src/main/java/pe/edu/upc/spring/ServletInitializer.java
+++ src/main/java/pe/edu/upc/spring/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WashFastSpApplication.class);
+		return application.sources(WashFastCodeV1Application.class);
 	}
 
 }
